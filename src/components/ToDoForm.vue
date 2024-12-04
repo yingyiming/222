c42072ab00cefd9b920da4e7148ec982ec55bfe6
尹一鸣 <1754427442@qq.com>
2024-12-04 15:59:57 +0800

    提交信息

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <h2 class="label-wrapper">
+      <label for="new-todo-input" class="label__lg">
+        What needs to be done?
+      </label>
+    </h2>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model.lazy.trim="label"
+      class="input__lg" />
+    <button type="submit" class="btn btn__primary btn__lg">Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      label: "",  // data 应该放在这里
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.label === "") {
+        return;
+      }
+      this.$emit("todo-added", this.label);
+      this.label = ""; // 清空输入框
+    },
+  },
+};
+</script>
